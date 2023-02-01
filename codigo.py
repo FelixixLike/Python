a2ea0225775a47ac54a8c24f2e3f36b230088f1c
Felixix L_X <103299060+FelixixLike@users.noreply.github.com>
2023-02-01 14:56:12 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,1 +1,33 @@
-print("hy")
+class Persona:
+    def __init__(self,nombre,apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+    pass
+
+class Cliente(Persona):
+    def __init__(self,nombre,apellido,numero_cuenta,balance):
+        super().__init__(nombre,apellido)
+        self.numero_cuenta =numero_cuenta
+        self.balance = balance
+
+    def depositar(self,cantidad):
+        self.balance += cantidad
+        
+
+    def retirar(self,cantidad):
+        self.balance -= cantidad
+
+
+def crear_cliente():
+    cliente = Cliente("juan", "peres", 12, 5000)
+
+
+
+def inicio(cliente):
+    pass
+
+cliente = Cliente("f", "f", 5, 5)
+
+cliente =Cliente("juan", "peres", 12, 5000)
+
+print(cliente.balance)
